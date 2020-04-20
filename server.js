36cd6b736f20306071ee0b68549695adc04c8d2a
unknown <esankhan3@gmail.com>
2020-04-20 22:08:12 +0530

    Adding Compression library

--- server.js
+++ server.js
@@ -1,50 +1,46 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const path = require('path')
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+const compression = require("compression");
 
-
-if(process.env.NODE_ENV!=='production'){
-    require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const stripe  = require('stripe')(process.env.STRIPE_ACCOUNT_SECRET_KEY)
+const stripe = require("stripe")(process.env.STRIPE_ACCOUNT_SECRET_KEY);
 
+const app = express();
 
-const app =express()
+const port = process.env.PORT || 5000;
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-const port = process.env.PORT||5000
+app.post("/payment", (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: "usd",
+  };
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
+});
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(cors())
-
-app.post('/payment',(req,res)=>{
-    const body = {
-        source:req.body.token.id,
-        amount:req.body.amount,
-        currency:'usd'
-    };
-    stripe.charges.create(body,(stripeErr,stripeRes)=>{
-        if(stripeErr){
-            res.status(500).send({error:stripeErr})
-        }else {
-            res.status(200).send({success:stripeRes})
-        }
-    })
-
-})
-
-if(process.env.NODE_ENV==='production'){
-    app.use(express.static(path.join(__dirname,'client/build')))
-    app.get('*',function(req,res){
-        res.sendFile(path.join(__dirname,'client/build','index.html'))
-    })
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
 }
 
-app.listen(port,error=>{
-    if(error) throw error;
-    console.log(`Server running on port ${port}`)
-})
-
+app.listen(port, (error) => {
+  if (error) throw error;
+  console.log(`Server running on port ${port}`);
+});
