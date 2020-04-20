670ab6e7649f55012c25a7b87555287511130824
unknown <esankhan3@gmail.com>
2020-04-20 22:36:51 +0530

    Added PWA compliant, Added Serviceworker

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./store/store";
+import * as serviceWorker from "./serviceWorker";
+
 import "./index.css";
 import App from "./App";
 
@@ -17,3 +19,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+
+serviceWorker.register();
